f7680ab53bb05c575241cfa7e0fb485b1cf32796
ajibroziq21@gmail.com <ajibroziq21@gmail.com>
2023-04-18 21:16:07 +0700

    mass assign

---
+++ resources/views/layouts/partials/navbar.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+    <a href="" class="navbar-brand p-0">
+        <h1 class="m-0">SiAbanglih</h1>
+        <!-- <img src="img/logo.png" alt="Logo"> -->
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="fa fa-bars"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav mx-auto py-0">
+            <a href="#home" class="nav-item nav-link active">Home</a>
+            <a href="#kandidat" class="nav-item nav-link">Kandidat & Peserta</a>
+            <a href="#qc" class="nav-item nav-link">Quick Count</a>
+            <a href="#voting" class="nav-item nav-link">Voting</a>
+        </div>
+        <a href="" class="btn btn-primary-gradient rounded-pill py-2 px-4 ms-3 d-none d-lg-block">Login</a>
+    </div>
+</nav>
